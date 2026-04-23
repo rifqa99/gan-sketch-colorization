a3bc96293586d49be85f0f5106ca02d96cb6a617
Rifqa <rifqa99@users.noreply.github.com>
2026-04-24 00:17:28 +0300

    Initial project structure

---
+++ src/datasets/dataset_loader.py
@@ -0,0 +1,35 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+
+class Edges2ShoesDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.image_files = sorted(os.listdir(root_dir))
+
+        if transform is None:
+            self.transform = transforms.Compose([
+                transforms.Resize((256, 512)),
+                transforms.ToTensor(),
+            ])
+        else:
+            self.transform = transform
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        img_name = self.image_files[idx]
+        img_path = os.path.join(self.root_dir, img_name)
+
+        image = Image.open(img_path).convert("RGB")
+        image = self.transform(image)
+
+        _, H, W = image.shape
+
+        input_image = image[:, :, :W // 2]
+        target_image = image[:, :, W // 2:]
+
+        return input_image, target_image
